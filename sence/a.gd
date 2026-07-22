e826c33b01478563d40a96b161e336a3bb3ca141
nex916434-cpu <nex916434@gmail.com>
2026-07-23 00:31:28 +0400

    test

--- sence/a.gd
+++ sence/a.gd
@@ -5,4 +5,4 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	if (body.name == "player"):
-		get_tree().reload_current_scene()
+		get_tree().change_scene_to_file("res://lose.tscn")
