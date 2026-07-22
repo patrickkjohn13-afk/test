7b5da885b16e1953e0653ecfcccaf217f8a3144c
nex916434-cpu <nex916434@gmail.com>
2026-07-23 00:47:19 +0400

    tets

--- lose.gd
+++ lose.gd
@@ -7,3 +7,8 @@ extends Control
 
 func _on_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://sence/main.tscn")
+
+
+
+func _on_button_4_pressed() -> void:
+	get_tree().change_scene_to_file("res://sence/level.tscn")
