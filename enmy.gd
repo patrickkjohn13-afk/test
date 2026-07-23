4082603791a87aaf24fd6a8d23d1677ef8cb7651
nex916434-cpu <nex916434@gmail.com>
2026-07-23 17:34:53 +0400

    djf

--- enmy.gd
+++ enmy.gd
@@ -3,29 +3,29 @@ extends CharacterBody2D
 @export var speed := 50.0
 var direction := 1
 
-@onready var wall_ray: RayCast2D = $FloorRay
 @onready var floor_ray: RayCast2D = $FloorRay2
 @onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
-
-func turn():
-	direction *= -1
-
-	wall_ray.target_position.x *= -1
-	floor_ray.position.x *= -1
-
-	sprite.flip_h = direction < 0
+@onready var floor_ray_3: RayCast2D = $FloorRay3
 
 func _physics_process(delta):
 	if !is_on_floor():
 		velocity += get_gravity() * delta
 
-	# إذا اصطدم بحائط
-	if wall_ray.is_colliding():
-		turn()
+	if is_on_wall() or !floor_ray.is_colliding():
+		direction *= -1
+		floor_ray.position.x *= -1
+		sprite.flip_h = direction < 0
 
-	# إذا لم يجد أرضًا أمامه
-	if !floor_ray.is_colliding():
-		turn()
+	velocity.x = -speed * direction
+	move_and_slide()
 
-	velocity.x = speed * direction
+	if !is_on_floor():
+		velocity += get_gravity() * delta
+
+	if is_on_wall() or !floor_ray_3.is_colliding():
+		direction *= -1
+		floor_ray_3.position.x *= -1
+		sprite.flip_h = direction < 0
+
+	velocity.x = -speed * direction
 	move_and_slide()
